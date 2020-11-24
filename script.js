f231e8a78642070d9e5042044c313631e1f92be2
Pedro Alburqueque <prolsu88@gmail.com>
2020-11-23 22:53:56 -0500

    added project/portfolio descriptions

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+    // <a href="./index.html" class="intLink">About Me</a>
+    //       <a href="./portfolio.html" class="intLink">Portfolio</a>
+    //       <a href="./contact.html" class="intLink">Contact</a>
+    //       <a class="icons" href="https://www.linkedin.com/in/pralburco/" target="_blank"><img src="./pictures/LI-In-Bug.png" alt="linkedin"></a>
+    //       <a class="icons" href="https://github.com/prolsu" target="_blank"><img src="./pictures/iconfinder_Github_1298743.png" alt="github"></a>
+
+    // $(".myLinks").append()
+
+    // $(".myLinks").on("click",".intLink",function(){
+    //     $(this).addClass("active");
+    // })
+
+
+})
