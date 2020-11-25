cbf6213ab98ad20052a32c6cfe0d9cd90157b07f
Pedro Alburqueque <prolsu88@gmail.com>
2020-11-24 22:51:37 -0500

    working on home text transition

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 $(document).ready(function(){
     
-    // <a href="./index.html" class="intLink">About Me</a>
-    //       <a href="./portfolio.html" class="intLink">Portfolio</a>
-    //       <a href="./contact.html" class="intLink">Contact</a>
-    //       <a class="icons" href="https://www.linkedin.com/in/pralburco/" target="_blank"><img src="./pictures/LI-In-Bug.png" alt="linkedin"></a>
-    //       <a class="icons" href="https://github.com/prolsu" target="_blank"><img src="./pictures/iconfinder_Github_1298743.png" alt="github"></a>
-
-    // $(".myLinks").append()
-
-    // $(".myLinks").on("click",".intLink",function(){
-    //     $(this).addClass("active");
-    // })
-
+    $(".active").on("click",function(){
+        $("#desktopHeader").addClass("move");
+        $("#brief").addClass("showMe");
+        // $("#brief").attr("style","opacity:1;")
+    })
 
 })
