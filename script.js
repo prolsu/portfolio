38eacf715a2db620ba26ad0100d91726073bdf36
Pedro Alburqueque <prolsu88@gmail.com>
2020-11-26 04:13:49 -0500

    updated style in portfolio page

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 $(document).ready(function(){
     
     $(".active").on("click",function(){
-        $("#desktopHeader").addClass("move");
+
         $("#brief").addClass("showMe");
-        // $("#brief").attr("style","opacity:1;")
+        $("#desktopHeader").addClass("move");
     })
+
+    $("#hwHolder1").hover(
+        function(){
+            $("#hwHolder2").addClass("after");
+            $("#hwHolder3").addClass("after");
+        },
+        function(){
+            $("#hwHolder2").removeClass("after");
+            $("#hwHolder3").removeClass("after");    
+        }
+    );
+
+    $("#hwHolder2").hover(
+        function(){
+            $("#hwHolder1").addClass("after");
+            $("#hwHolder3").addClass("after");
+        },
+        function(){
+            $("#hwHolder1").removeClass("after");
+            $("#hwHolder3").removeClass("after");    
+        }
+    );
+
+    $("#hwHolder3").hover(
+        function(){
+            $("#hwHolder1").addClass("after");
+            $("#hwHolder2").addClass("after");
+        },
+        function(){
+            $("#hwHolder1").removeClass("after");
+            $("#hwHolder2").removeClass("after");    
+        }
+    );
 
 })
